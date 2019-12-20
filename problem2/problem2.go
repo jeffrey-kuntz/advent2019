2b91fc20aee30bd41f302bf3f4a8d7f7a4eedbb1
Jeffrey Kuntz <jeffrey.kuntz@gmail.com>
2019-12-20 11:59:44 -0500

    Updated my case statement.

--- problem2/problem2.go
+++ problem2/problem2.go
@@ -2,6 +2,7 @@ package problem2
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -33,37 +34,35 @@ func LoadOptComputer(filename string) ([]int, error) {
 }
 
 // RunOptsCodes runs the codes
-func RunOptsCodes(inCodes []int) []int {
+func RunOptsCodes(inCodes []int) ([]int, error) {
 	codes := append([]int(nil), inCodes...)
 
 	for i := 0; i < len(codes); i += 4 {
+		if codes[i] == 99 {
+			// Done
+			fmt.Println("done")
+			return codes, nil
+		}
+
+		position1 := codes[i+1]
+		value1 := codes[position1]
+
+		position2 := codes[i+2]
+		value2 := codes[position2]
+
+		returnPosition := codes[i+3]
+
 		switch codes[i] {
 		case 1:
 			// Addition
-			position1 := codes[i+1]
-			value1 := codes[position1]
-
-			position2 := codes[i+2]
-			value2 := codes[position2]
-
-			returnPosition := codes[i+3]
 			codes[returnPosition] = value1 + value2
 		case 2:
 			// Multiplication
-			position1 := codes[i+1]
-			value1 := codes[position1]
-
-			position2 := codes[i+2]
-			value2 := codes[position2]
-
-			returnPosition := codes[i+3]
 			codes[returnPosition] = value1 * value2
-		case 99:
-			// Done
-			fmt.Println("done")
-			return codes
+		default:
+			return nil, errors.New("unrecognized code: " + string(codes[i]))
 		}
 	}
 
-	return codes
+	return codes, nil
 }
